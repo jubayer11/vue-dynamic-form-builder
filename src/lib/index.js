207dc598f179d07ddc3de0c8a06f56eed0f6c500
Jubayer Ahmed <jubayer@Jubayers-MacBook-Air.local>
2025-04-30 12:15:00 +0600

    fix: correct entry point

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,5 +2,6 @@ import DynamicForm from '@/components/DynamicForm/index.vue';
 export { FieldType } from '@/utils/dynamicForm/FieldTypeClass';
 export { CustomizableTextField } from '@/utils/dynamicForm/CustomizableTextFieldClass';
 export { ValidationSetup } from '@/utils/dynamicForm/ValidationSetupClass';
+import { fieldType } from "@/utils/dynamicForm/customizableTextField.js";
 
 export default DynamicForm;
